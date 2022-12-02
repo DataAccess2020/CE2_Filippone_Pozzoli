e8c150d46ca51d7dd30f59f617fce108ae2b1ef2
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-12-02 12:48:06 +0100

    adds comments

--- Scripts/pagedownload.R
+++ Scripts/pagedownload.R
@@ -1,7 +1,13 @@
+# We start by loading the RCurl package.
+
 library(RCurl)
+
+# We assign the URL of the website we are interested in to an object called
+# "url".
 
 url <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
 
+# We download the webpage politely, indicating out email and our R version.
+
 resp <- getURL(url=url, httpheader= c(from ="cristina.pozzoli@icloud.com", 
                               'User-Agent' = R.Version()))
-

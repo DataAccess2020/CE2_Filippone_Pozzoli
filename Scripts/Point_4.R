8ad37aa4d000e2197679d684f8f35c960eca94b5
cristina-pozzoli <cristina.pozzoli@icloud.com>
2022-12-03 14:56:31 +0100

    Adds a script for the point 4

---
+++ Scripts/Point_4.R
@@ -0,0 +1,7 @@
+# We download the webpage 
+
+url2 <- "https://beppegrillo.it/category/archivio/2016/"
+
+resp2 <- getURL(url=url2, httpheader= c(from ="cristina.pozzoli@icloud.com", 
+                                      'User-Agent' = R.Version()))
+resp2

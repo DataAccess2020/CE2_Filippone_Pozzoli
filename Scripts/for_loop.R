0e5c94f6a0f9a55d8e0d29a57de0ff931f6440c2
cristina-pozzoli <cristina.pozzoli@icloud.com>
2022-12-04 10:47:21 +0100

    adds the function to download the pages

--- Scripts/for_loop.R
+++ Scripts/for_loop.R
@@ -7,3 +7,30 @@ library(tidyverse)
 webpages <- str_c("https://beppegrillo.it/category/archivio/2016/page/", 1:47) %>%
   paste0("/")
 webpages
+
+# We create the 'download_politely' function
+
+download_politely <- function(from_url, to_html, my_email, my_agent = R.Version()$version.string) {
+  
+  require(httr)
+  
+  # Check that arguments are inputted as expected:
+  stopifnot(is.character(from_url))
+  stopifnot(is.character(to_html))
+  stopifnot(is.character(my_email))
+  
+  # GET politely
+  simps_req <- httr::GET(url = from_url, 
+                         add_headers(
+                           From = my_email, 
+                           `User-Agent` = R.Version()$version.string
+                         )
+  )
+  # If status == 200, extract content and save to a file:
+  if (httr::http_status(simps_req)$message == "Success: (200) OK") {
+    bin <- content(simps_req, as = "raw")
+    writeBin(object = bin, con = to_html)
+  } else {
+    cat("Houston, we have a problem!")
+  }
+}

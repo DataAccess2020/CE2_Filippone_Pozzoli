a3d171125e50fd6ac9dda45873aedf9049acbfa9
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-12-04 18:14:51 +0100

    adds Rmarkdown files and modifies script names

---
+++ Scripts/02.html_links.R
@@ -0,0 +1,26 @@
+# We download all the HTML links from the webpage 
+
+library(XML)
+links <- getHTMLLinks(doc=resp, externalOnly = T, relative = F)
+links
+
+# We organize all the links in a data frame.
+
+links2<-as.data.frame(links)
+
+#Now we extract only the links which point to other posts of the beppegrillo.it
+
+link_unl <- unlist(links2)
+
+library(stringr)
+
+str_view_all(link_unl, "https://beppegrillo\\.it/.+")
+
+links_blog <- str_extract_all(link_unl, "https://beppegrillo\\.it/.+")
+links_blog_unl <- unlist(links_blog)
+links_blog1 <- as.data.frame(links_blog_unl)
+
+
+links_blog2 <- str_extract_all(link_unl, "https://beppegrillo\\.it/[^category].+")
+links_blog_unl2 <- unlist(links_blog2)
+links_blog2<- as.data.frame(links_blog_unl2)
---
+++ Scripts/05.Scraping_posts .R
@@ -0,0 +1,20 @@
+# SCREAPING TEXT
+
+# We scrape all the links to the blog posts 
+to_scrape <- list.files(here::here("folder_beppe"), full.names = TRUE)
+all_html_2016 <- vector(mode = "list", length = length(to_scrape))  
+
+for (i in seq_along(all_html_2016)){
+  all_html_2016[[i]] = XML::getHTMLLinks(to_scrape[i], externalOnly = T)
+  
+  Sys.sleep(0.5)
+}
+
+# We select only the links to the posts 
+
+all_html_2016
+all_html_2016_unl <- unlist(all_html_2016)
+links <- str_extract_all(all_html_2016_unl, "https://beppegrillo\\.it/[^category][^jpg].+")
+links_unl <- unlist(links)
+links_unl
+links_data <- as.data.frame(links_unl)
